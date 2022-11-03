a79e9e1f22b3812b4e8865f3851d3a7b796963d3
Pravali777 <pravallika.af686@gmail.com>
2022-11-02 22:01:39 -0500

    add

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 var PeacockRouter = require('./routes/Peacock');
 var gridbuildRouter = require('./routes/gridbuild');
-
+var selectorRouter = require('./routes/selector');
 
 var app = express();
 
@@ -26,6 +26,7 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/Peacock', PeacockRouter);
 app.use('/gridbuild', gridbuildRouter);
+app.use('/selector', selectorRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
